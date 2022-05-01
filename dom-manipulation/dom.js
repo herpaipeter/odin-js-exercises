de9d527a1dc5cf09b41f4b7058375715a1c13006
Herpai Péter <herpaip@datapress.hu>
2022-05-01 12:54:34 +0200

    Add dom manipulation red text

---
+++ dom-manipulation/dom.js
@@ -0,0 +1,6 @@
+const container = document.querySelector("#container");
+
+const redP = document.createElement("p");
+redP.style.color = "red";
+redP.textContent = "Hey I’m red!";
+container.appendChild(redP);
